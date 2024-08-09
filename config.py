1ec37ff1547e795e7f14ff769218e9a8b0a11a72
cbxroman <ashifm666@gmail.com>
2024-08-09 09:24:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7439672972:AAHJ3tXhEpT0PsiMD43-anC9VTT9xn0uluA")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("flash_hasan")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6483192993")
+
+
+
+
+
+
+
+  
